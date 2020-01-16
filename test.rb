ed1070b3b15a703204d73640bf883cbbf6b189c6
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-01-16 13:20:26 +0530

    Find element id and raise the click event

--- test.rb
+++ test.rb
@@ -1,5 +1,19 @@
 require 'selenium-webdriver'
 driver = Selenium::WebDriver.for :chrome
-driver.navigate.to  'http://algorisys.com'
-driver.manage.window.maximize
+
+driver.navigate.to 'http://lazyautomation.co.uk/lazy1.html'  # 'http://algorisys.com'
+
+driver.manage.window.maximize # maximize window
+
+
+# Find element and click
+
+driver.find_element(:id, 'hello1').click
+
+sleep(5)  # sleep for 5 seconds
+
+
+
+
+
 
